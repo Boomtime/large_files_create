4d726cd27eeb8d745609149af5c52ba0479bad03
Boomtime <andrew.ryder@10gen.com>
2014-08-26 11:47:17 +1000

    Create large empty files on NTFS really quickly
    Do you have too much disk space? Do you wish for the old days when
    individual files consumed significant portions of your total disk size?
    Well good news! This utility will let you create large, usually
    contiguous, files of arbitrary size. So easy to use you'll think it's
    1985 all over again.

---
+++ large_files_create.cpp
@@ -0,0 +1,98 @@
+/*
+ * create large empty files in Windows
+ * only works on NTFS, probably
+ */
+
+#include <Windows.h>
+#include <tchar.h>
+
+#define DPRINT(x,...) _tprintf(_T(x) _T("\n"),__VA_ARGS__)
+
+
+/* MongoDB maximum extents file size */
+#define MONGODB_MAX_FILE_SIZE  2146435072
+
+/* create a file handle for use as a large file read/write test */
+static HANDLE createFile( const TCHAR* p_fn )
+{
+	return CreateFile(
+		p_fn,
+		FILE_READ_ACCESS | FILE_WRITE_ACCESS,
+		0, /* share mode */
+		NULL, /* security descriptor */
+		CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL | FILE_FLAG_RANDOM_ACCESS,
+		0 /* template */
+	);
+}
+
+/* set file size to p_size bytes beyond current file pointer position */
+static void fileInflate( HANDLE p_file, DWORD p_size )
+{
+	SetFilePointer( p_file, p_size, 0, FILE_CURRENT );
+    SetEndOfFile( p_file );
+}
+
+int _tmain( int argc, _TCHAR* argv[] )
+{
+	HANDLE fd;
+	unsigned long long fsize;
+	TCHAR* end;
+	int apos;
+
+	DPRINT( "large_files_create:" );
+
+	if( argc <= 1 )
+	{
+		DPRINT( " nothing to do... specify some file names perhaps" );
+		return 1;
+	}
+
+	end = 0;
+	fsize = _tcstoui64( argv[1], &end, 0 );
+
+	/* check if no conversion */
+	if( end == argv[1] )
+	{
+		fsize = MONGODB_MAX_FILE_SIZE;
+		apos = 1;
+		DPRINT( " size defaulting to [%i]", fsize );
+	}
+	else
+	{
+		apos = 2;
+		DPRINT( " size parsed as [%i]", fsize );
+	}
+
+	DPRINT( " file count is [%i]", argc - apos );
+
+	while( apos < argc )
+	{
+		if( _tcslen( argv[apos] ) > 0 )
+		{
+			DPRINT( " try create [%s]", argv[apos] );
+
+			fd = createFile( argv[apos] );
+
+			if( 0 == fd )
+			{
+				DPRINT( " FAILED [%i]", GetLastError() );
+				return 2;
+			}
+			else
+			{
+				DPRINT( " file opened [%08X]", fd );
+
+				fileInflate( fd, fsize );
+				DPRINT( " file inflated", fd );
+
+				CloseHandle( fd );
+			}
+		}
+
+		apos++;
+	}
+
+	return 0;
+}
+
